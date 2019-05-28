333b7976890eb3367fa76db03efa56c394313543
Thiago Damasceno <thiago.damasceno@corp.globo.com>
2019-05-28 18:53:25 -0300

    funcionando antes de ir pra generics

---
+++ Example/ViewCellMatchAndBanner/Cell/CollectionViewCellExample.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCellExample.swift
+//  ViewCellMatchAndBanner_Example
+//
+//  Created by Thiago Outeiro Pereira Damasceno on 24/05/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCellExample: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
